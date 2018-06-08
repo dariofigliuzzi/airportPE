e871c93035422a4222016c3a81879c4445a95e98
Dario <dariofigliuzzi>
2018-06-08 14:28:24 +0200

    plane_queue changed in waiting_queue (Tower.cc)

--- src/Tower.cc
+++ src/Tower.cc
@@ -31,7 +31,7 @@ cMessage *temp_msg_takeoff; //per invio OK a Takeoff
 bool free;
 
 //nuovo approccio
-cQueue plane_queue;
+cQueue waiting_queue;
 
 void Tower::initialize()
 {
@@ -58,8 +58,8 @@ void Tower::handleMessage(cMessage *msg)
 
    else if(p)
    {
-       plane_queue.insert(p);
-       EV<< "TOWER: Aereo aggiunto nella relativa coda\n";
+       waiting_queue.insert(p);
+       EV<< "TOWER: Aereo aggiunto nella coda di attesa per pista\n";
 
        if(free) send_OK();
        return;
@@ -70,14 +70,14 @@ void Tower::send_OK() {
     cObject* obj_plane = nullptr;
     Plane* tmp_plane;
 
-    if(!plane_queue.isEmpty())
+    if(!waiting_queue.isEmpty())
     {
-        obj_plane = plane_queue.front();
+        obj_plane = waiting_queue.front();
         tmp_plane = dynamic_cast<Plane*>(obj_plane);
 
-        EV << "TOWER: PRINTING PLANE QUEUE:\n";
+        EV << "TOWER: PRINTING WAITING QUEUE:\n";
 
-        for(cQueue::Iterator iter(plane_queue,0); !iter.end(); iter++)
+        for(cQueue::Iterator iter(waiting_queue,0); !iter.end(); iter++)
         {
             Plane* myMsg =(Plane*) iter.operator *();
             EV <<"- ID:" <<myMsg->getId() << "  ENTRY(s):" << myMsg->getEnter() << "  KIND:" << myMsg->getKind() << "\n"; //0:Landing 1:Takeoff
@@ -88,7 +88,7 @@ void Tower::send_OK() {
             EV << "TOWER: Faccio atterrare in seguito a pista libera\n";
             cMessage *tmp_msg = new cMessage("OK");//sfrutto questo messaggio
             send(tmp_msg, "out_land");
-            plane_queue.remove(plane_queue.front());
+            waiting_queue.remove(waiting_queue.front());
             return;
         }
         else if(tmp_plane->getKind() == 1) { //aereo in arrivo da takeoff
@@ -96,7 +96,7 @@ void Tower::send_OK() {
             EV << "TOWER: Faccio decollare in seguito a pista libera\n";
             cMessage *tmp_msg = new cMessage("OK");//sfrutto questo messaggio
             send(tmp_msg, "out_takeoff");
-            plane_queue.remove(plane_queue.front());
+            waiting_queue.remove(waiting_queue.front());
             return;
         }
     }
