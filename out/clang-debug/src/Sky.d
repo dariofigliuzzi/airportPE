f66625bd55387bdca04e9552f560d6e54e757e39
dario <dariofigliuzzi>
2019-02-07 16:38:49 +0100

    result and ini files

--- out/clang-debug/src/Sky.d
+++
@@ -1,8 +0,0 @@
-../out/clang-debug/src/Sky.o: Sky.cc Sky.h \
-  C:/omnetpp-5.3/include\omnetpp.h Plane_m.h
-
-Sky.h:
-
-C:/omnetpp-5.3/include\omnetpp.h:
-
-Plane_m.h:
--- out/clang-debug/src/Tower.d
+++
@@ -1,8 +0,0 @@
-../out/clang-debug/src/Tower.o: Tower.cc Tower.h \
-  C:/omnetpp-5.3/include\omnetpp.h Plane_m.h
-
-Tower.h:
-
-C:/omnetpp-5.3/include\omnetpp.h:
-
-Plane_m.h:
